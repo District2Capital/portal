46cda6c3ca733449162695804ee75a7be175828b
Andreas Bigger <abigger87@gmail.com>
2019-06-21 10:23:33 -0400

    removed node modules and add some heroku :party:

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Serve Static Assets - Heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('../client/build'));
+}
+
 require('./startup/logging')(app);
 require('dotenv').config();
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/routes')(app);
 
-app.set('port', process.env.API_PORT || 3001);
+app.set('port', process.env.PORT || process.env.API_PORT || 3001);
 
 app.listen(app.get('port'), 'localhost', function () {
   var host = this.address().address;
