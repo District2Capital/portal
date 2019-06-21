26fb167e4e61bffb06728a1a2919c869206bce77
Andreas Bigger <abigger87@gmail.com>
2019-06-21 15:48:54 -0400

    post request fix

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const winston = require('winston');
 module.exports = function () {
     const dbRoute =
         `mongodb+srv://${process.env.D2C_MONGODB_USERNAME}:${process.env.D2C_MONGODB_PASSWORD}@portal-twav4.mongodb.net/test?retryWrites=true&w=majority`;
-    //`mongodb://${process.env.D2C_MONGODB_USERNAME}:${process.env.D2C_MONGODB_PASSWORD}@ds231377.mlab.com:31377/d2c`;
+    const mlabRoute = `mongodb://${process.env.D2C_MONGODB_USERNAME}:${process.env.D2C_MONGODB_PASSWORD}@ds231377.mlab.com:31377/d2c`;
     mongoose.connect(dbRoute, { useNewUrlParser: true });
 
     let db = mongoose.connection;
